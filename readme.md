1576d4a107b04710dc839358ff99a8dd5a92eb0f
80rokwoc4j <80rokwoc4j@gmail.com>
2022-10-23 13:38:31 +0900

    now bot reply in thread

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
+invite bot or run it your self  
+https://discord.com/oauth2/authorize?client_id=1032354931673407620&permissions=19327372288&scope=bot
+
+
 how to run
 ```
 git clone https://github.com/80ROkWOC4j/discord_search_bot.git
@@ -7,10 +11,11 @@ docker run -d --restart=unless-stopped -e DISCORD_TOKEN="YOUR TOKEN" --name dsb 
 ```
 
 how to use
-1. invite bot.
+1. invite bot. you need read msg, send msg, manage threads, use slash command permission.
 2. type mention bot register.  
 ```@SearchBot register```
 3. bot gonna reply with buttons. click "Register in guild" button.
+
 
 usage
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-use poise::serenity_prelude::{self as serenity, Activity};
+use std::{vec};
+
+use poise::{serenity_prelude::{self as serenity, Activity, CreateButton}};
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
-// User data, which is stored and accessible in all command invocations
+
 struct Data {}
 
-/// Displays your or another user's account creation date
+fn search_more_button () -> CreateButton {
+    let mut button = CreateButton::default();
+    button.custom_id("search more");
+    button.label("search more");
+    button.style(serenity::ButtonStyle::Primary);
+    button
+}
+
 #[poise::command(slash_command, prefix_command)]
 async fn search(
     ctx: Context<'_>,
     #[description = "text to search"] text: String,
-    #[description = "검색할 최근 채팅 갯수(기본 200)"] count: Option<u64>,
+    #[description = "count of recent chat for search(default 200)"] count: Option<u64>,
+    #[description = "max length of shown messages(default 200)"] shown_length: Option<u64>,
 ) -> Result<(), Error> {
     ctx.discord().set_activity(Activity::playing(&text)).await;
     let typing = serenity::Typing::start(ctx.discord().http.clone(), ctx.channel_id().0)?;
@@ -19,18 +29,44 @@ async fn search(
         None => 200,
         Some(i) => i,
     };
-    let _messages = ctx.channel_id().messages(&ctx.discord(), |retriever| retriever.limit(limit)).await?;
-    let mut result = text.clone().to_owned() + " 검색 결과 \n";
-    for history in _messages {
-        if history.content.contains(&text) {
-            result += &history.link();
-            result += "\n";
+    // max size of discord embed msg is 1024
+    let shown_len = match shown_length {
+        None => 200,
+        Some(i) => i,
+    };
+    let messages = ctx.channel_id().messages(&ctx.discord(), |retriever| retriever.limit(limit)).await?;
+    
+    let result_msg = "Search Result of ".to_string() + &text;
+
+    let reply_id = ctx.say(&result_msg).await?.into_message().await?.id;
+
+    let thread = ctx.channel_id().create_public_thread(&ctx.discord().http, reply_id, |t| t.name(&result_msg)).await.unwrap();
+
+    for msg in messages {
+        if msg.content.contains(&text) {
+            thread.send_message(&ctx.discord().http, |b| {
+                let name = format!("{}\t{}", &msg.author.name, &msg.timestamp.date().to_string());
+                let value:String;
+                if msg.content.len() > shown_len.try_into().unwrap() {
+                    value = format!("[{}]({})\n", &msg.content[..shown_len.try_into().unwrap()], &msg.link());
+                } else {
+                    value = format!("[{}]({})\n", &msg.content, &msg.link());
+                }
+                b.embed(|e| e.field(&name, &value, false))
+            }).await?;
         }
     }
 
-    // let u = user.as_ref().unwrap_or_else(|| ctx.author());
-    // let response = format!("{}'s account was created at {}", u.name, u.created_at());
-    ctx.say(&result).await?;
+    thread.send_message(&ctx.discord().http, |b| {
+        b.components(|c| {
+            c.create_action_row(|row| 
+                row.add_button(search_more_button())
+            )
+        })
+    }).await?;
+
+    // todo : make button works
+
     serenity::Typing::stop(typing);
     ctx.discord().set_activity(Activity::playing("/search")).await;
     Ok(())
